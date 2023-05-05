faaa6ecfefe227e13eadc0f0cdabba5199fde143
mingadinga <minpearl0826@gmail.com>
2023-05-05 14:26:02 +0900

    9. Spring JDBC 자동 구성 개발

---
+++ src/test/java/tobyspring/helloboot/jdbcTemplateTest.java
@@ -0,0 +1,34 @@
+package tobyspring.helloboot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HelloBootTest
+public class jdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        assertThat(count).isEqualTo(0);
+
+        jdbcTemplate.update("insert into hello values(?, ?)", "Toby", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+        count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+
+
+}
